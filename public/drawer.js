0cb9bfbb16a0889f9f0c7fea5abf6c637da61104
Sam Asghari <sam.asghari@gatech.edu>
2012-05-01 22:48:04 -0400

    changes to cheker appearance and selected/potential highlighting

--- public/drawer.js
+++ public/drawer.js
@@ -50,9 +50,10 @@ function Drawer(s) {
     this.drawingContext.beginPath();
     this.drawingContext.lineWidth =  1;
  
-    // board triangles, decoration
+    this.drawingContext.save();
+    // board triangles
     for (var x = 0; x <= this.specs.pixelWidth; x += this.specs.pieceWidth) {
-      this.drawingContext.save();
+
       this.drawingContext.globalAlpha = .3;
       this.drawingContext.strokeStyle = "black";
       
@@ -62,19 +63,17 @@ function Drawer(s) {
       this.drawingContext.lineTo(x + this.specs.pieceWidth/2, this.specs.maxPiecesPerTriangle * this.specs.pieceHeight);
       this.drawingContext.lineTo(x + this.specs.pieceWidth, 0);
       this.drawingContext.lineTo(x, 0);
-      
-      //botton triangles
       this.drawingContext.stroke();
+    
+      //botton triangles
       this.drawingContext.beginPath();
       this.drawingContext.moveTo(x, this.specs.pixelHeight);
       this.drawingContext.lineTo(x + this.specs.pieceWidth/2, this.specs.pixelHeight - (this.specs.maxPiecesPerTriangle * this.specs.pieceHeight));
       this.drawingContext.lineTo(x + this.specs.pieceWidth, this.specs.pixelHeight);
       this.drawingContext.lineTo(x, this.specs.pixelHeight);
       this.drawingContext.stroke();
-      
-      this.drawingContext.restore();
     }    
-
+    this.drawingContext.restore();
     
     /* draw it! */
     this.drawingContext.strokeStyle = "#ccc";
@@ -91,18 +90,6 @@ function Drawer(s) {
     for (var y = 0; y <= this.specs.pixelHeight; y += this.specs.pieceHeight) {
       this.drawingContext.fillRect(this.specs.boardWidth * this.specs.pieceWidth + 1, y, this.specs.bearOffWidth, this.specs.pieceHeight);
     }
-    
-    /* interact area... doubling dice, regular dice, etc. */
-    this.drawingContext.fillStyle = "#f0f";
-    //this.drawingContext.fillRect(this.interact.startX, this.interact.startY, this.interact.totalWidthPix,  this.interact.totalHeightPix);  
-    //this.drawingContext.fillStyle = "#D2B48C";
-    //this.drawingContext.fillRect(this.interact.doubling.startX, this.interact.doubling.startY, this.interact.doubling.widthPix,  this.interact.doubling.heightPix);    
-    //this.drawingContext.fillStyle = this.interact.dice.activeColor;
-    //this.drawingContext.fillRect(this.interact.dice.startX, this.interact.dice.startY, this.interact.dice.widthPix,  this.interact.dice.heightPix);  
-    //this.drawingContext.fillRect(this.interact.dice.startX + this.interact.dice.piecePadding +  0*(this.interact.dice.pieceWidth  + this.interact.dice.piecePadding), this.interact.dice.startY, this.interact.dice.pieceWidth, this.interact.dice.pieceHeight);
-    //this.drawingContext.fillRect(this.interact.dice.startX + this.interact.dice.piecePadding +  1*(this.interact.dice.pieceWidth  + this.interact.dice.piecePadding), this.interact.dice.startY, this.interact.dice.pieceWidth, this.interact.dice.pieceHeight);
-    //this.drawingContext.fillRect(this.interact.dice.startX + this.interact.dice.piecePadding +  2*(this.interact.dice.pieceWidth  + this.interact.dice.piecePadding), this.interact.dice.startY, this.interact.dice.pieceWidth, this.interact.dice.pieceHeight);
-    //this.drawingContext.fillRect(this.interact.dice.startX + this.interact.dice.piecePadding +  3*(this.interact.dice.pieceWidth  + this.interact.dice.piecePadding), this.interact.dice.startY, this.interact.dice.pieceWidth, this.interact.dice.pieceHeight);
   }
   
   this.drawDoublingDice = function(opts) {
@@ -141,22 +128,6 @@ function Drawer(s) {
                                   this.interact.dice.startX + this.interact.dice.piecePadding +  pos * (this.interact.dice.pieceWidth  + this.interact.dice.piecePadding) + ((this.interact.dice.pieceWidth - this.interact.dice.piecePadding )/2) , 
                                   this.interact.dice.startY + ( (this.interact.dice.pieceHeight + this.interact.padding) / 2 ));       
     }
-    
-    /*
-    // do we need to "highlight" to indicate an action? not right now
-    if ( opts.pCanConfirm || opts.pCanRoll ) {
-      if ( opts.pCanConfirm ) {
-        this.drawingContext.strokeStyle = opts.currentPlayer.color;
-      } else if ( opts.pCanRoll ) {
-        this.drawingContext.strokeStyle = opts.mePlayer.color; 
-      }    
- 
-      this.drawingContext.globalAlpha = 0.9;
-      this.drawingContext.lineWidth = 2;
-      this.drawingContext.strokeRect( this.interact.dice.startX + this.drawingContext.lineWidth, this.interact.dice.startY + this.drawingContext.lineWidth, 
-                                      this.interact.dice.widthPix - 2*this.drawingContext.lineWidth,  this.interact.dice.heightPix - 2*this.drawingContext.lineWidth );   
-    }
-    */
     this.drawingContext.restore();
   }
   
@@ -182,35 +153,29 @@ function Drawer(s) {
   }  
 
   this.highlight = function(space, width, isPotential) {
-    var color, l, base, height, tx;
-    var modNumCheckers = space.numCheckers > this.specs.maxPiecesPerTriangle ? this.specs.maxPiecesPerTriangle : space.numCheckers;
-    tx = space.column * this.specs.pieceWidth;
+    var base, x;
+    x = space.column * this.specs.pieceWidth;
     base = space.isTop() ? 0 : this.specs.pixelHeight;
-    l = ( space.type == "bearoff" ) ? this.specs.bearOffWidth : this.specs.pieceWidth;
-    isPotential ? color =  "#a020f0" : "#00ff00";
-    height = isPotential ? 0 : modNumCheckers * this.specs.pieceHeight;
-    if ( !space.isTop() ) {
-      height = this.specs.pixelHeight - height;
-    }
 
     this.drawingContext.beginPath();
-    this.drawingContext.moveTo( 0.5 + tx, base );
-    this.drawingContext.lineTo( 0.5 + tx, height );
-    this.drawingContext.lineTo( 0.5 + tx + l, height );
-    this.drawingContext.lineTo( 0.5 + tx + l, base );
+    this.drawingContext.moveTo(x, base);
+    this.drawingContext.lineTo(x + this.specs.pieceWidth/2, Math.abs(base - (this.specs.maxPiecesPerTriangle * this.specs.pieceHeight)));
+    this.drawingContext.lineTo(x + this.specs.pieceWidth, base);
+    this.drawingContext.lineTo(x, base);
+    this.drawingContext.stroke();
 
-    this.drawingContext.lineWidth = width;
-    this.drawingContext.strokeStyle = color;
+    this.drawingContext.lineWidth = 3;
+    this.drawingContext.strokeStyle = "#a020f0";
     this.drawingContext.stroke();      
   }
 
   this.highlightTriangles = function(selected, potentials) {
-    this.highlight(selected, 3, false);
+    this.highlightSelectedSpace(selected);
     for (var i = 0; i < potentials.length; i++) this.highlight(potentials[i], 3, true)
   }  
   
   this.highlightBars = function(selected, potentials) {
-    this.highlight(selected, 3, false);
+    this.highlightSelectedSpace(selected);
     for (var i = 0; i < potentials.length; i++) this.highlight(potentials[i], 3, true)
   }      
   
@@ -227,11 +192,19 @@ function Drawer(s) {
     }
     
     if (t.numCheckers > (thresh)) {
-      t.isTop() ? this.drawTextGTThresh( { column: t.column, row: thresh - 1, isTop: t.isTop(), numCheckers: t.numCheckers } ) : 
-                  this.drawTextGTThresh( { column: t.column, row: this.specs.boardHeight - thresh, isTop: t.isTop(), numCheckers: t.numCheckers } ) ;
+      t.isTop() ? this.drawThreshNum( { column: t.column, row: thresh - 1, isTop: t.isTop(), numCheckers: t.numCheckers } ) : 
+                  this.drawThreshNum( { column: t.column, row: this.specs.boardHeight - thresh, isTop: t.isTop(), numCheckers: t.numCheckers } ) ;
     }
   }  
 
+  this.drawThreshNum = function(opts) {
+    var x = (opts.column * this.specs.pieceWidth) + (this.specs.pieceWidth/2) - 5;
+    var y = (opts.row * this.specs.pieceHeight) + (this.specs.pieceHeight/2) + 5;  
+    this.drawingContext.font = "15pt Arial";
+    this.drawingContext.fillStyle = "white";
+    this.drawingContext.fillText(opts.numCheckers, x, y);
+  }  
+  
   this.drawBar = function(b) {
     for (var k = 0; k < b.numCheckers; k++) 
       b.isTop() ? this.drawPiece(new Checker(k, b.column, b.player), false) : this.drawPiece(new Checker(this.specs.boardHeight - k - 1, b.column, b.player), false);	
@@ -242,12 +215,10 @@ function Drawer(s) {
       b.isTop() ? this.drawBearPiece(new Checker(k * this.specs.bearOffHeight, b.column, b.player)) : this.drawBearPiece(new Checker((this.specs.boardHeight * this.specs.pieceHeight) - ((k+1) * this.specs.bearOffHeight) - 1, b.column, b.player));	
   }  
   
-  this.drawTextGTThresh = function(opts) {
-    var x = (opts.column * this.specs.pieceWidth) + (this.specs.pieceWidth/2) - 5;
-    var y = (opts.row * this.specs.pieceHeight) + (this.specs.pieceHeight/2) + 5;  
-    this.drawingContext.font = "15pt Arial";
-    this.drawingContext.fillStyle = "white";
-    this.drawingContext.fillText(opts.numCheckers, x, y);
+  this.highlightSelectedSpace = function(sp) {
+    sp.isTop() ? 
+      this.drawPiece(new Checker(sp.numCheckers - 1, sp.column, sp.player), true) : 
+      this.drawPiece(new Checker(this.specs.boardHeight - sp.numCheckers, sp.column, sp.player), true);
   }
   
   this.drawPiece = function(ch, selected) {
@@ -257,10 +228,20 @@ function Drawer(s) {
     this.drawingContext.beginPath();
     this.drawingContext.arc(x, y, radius, 0, Math.PI*2, false);
     this.drawingContext.closePath();
-    this.drawingContext.strokeStyle = "#000";
-    this.drawingContext.stroke();
-    ch.player == 1 ? this.drawingContext.fillStyle = this.specs.p1color : this.drawingContext.fillStyle = this.specs.p2color;
-    this.drawingContext.fill();   
+
+    // make radial glare
+    var color = ch.player == 1 ? this.specs.p1color : this.specs.p2color;
+    var grd = this.drawingContext.createRadialGradient(x, y, radius, x + 3, y - 5, radius-3);
+    grd.addColorStop(0, "white");
+    grd.addColorStop(1, color);
+    this.drawingContext.fillStyle = grd;
+    this.drawingContext.fill();
+    
+    if ( selected ) {
+      this.drawingContext.lineWidth = 4;
+      this.drawingContext.strokeStyle = "#0f0";
+      this.drawingContext.stroke();
+    }
   }
 
   this.drawBearPiece = function(ch) {
